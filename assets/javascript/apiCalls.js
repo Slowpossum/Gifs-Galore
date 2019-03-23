b0805980927a0c72132637bd3613e395b58e2824
Slowpossum <tsmithva@verizon.net>
2019-03-23 06:28:50 -0400

    Done.

--- assets/javascript/apiCalls.js
+++ assets/javascript/apiCalls.js
@@ -1,4 +1,4 @@
-var tags = JSON.parse(localStorage.getItem("tags")) || ["cat", "dog", "fish", "bird", "anime", "meme", "art"];
+var tags = JSON.parse(localStorage.getItem("tags")) || ["opossum", "cat", "dog", "fish", "bird", "anime", "meme", "art"];
 var favorites = JSON.parse(localStorage.getItem("favorites")) || {
     length: 0,
     list: []
@@ -10,23 +10,24 @@ var images = {
 var imagePosition = "empty";
 
 
+
 tags.forEach(tag => {
     createTag(tag);
 });
 
 if (favorites.length > 0) {
-    favorites[list].forEach(favorite => {
-        createFavorite(favorite);
+    favorites.list.forEach(favorite => {
+        createFavorite(favorites[favorite]);
     });
 }
 
-$(".tag").on("click", function () {
+$("#tagsDiv").on("click", ".tag", function () {
     var tag = $(this).val();
     if (images.offset[tag] === undefined) {
         images.offset[tag] = 0;
     }
 
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + tag + "&offset="+ images.offset[tag] +"&api_key=i3NTmSSVlFnAIZobSeDS4cGEcGvTjI1T&limit=5"
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + tag + "&offset=" + images.offset[tag] + "&api_key=i3NTmSSVlFnAIZobSeDS4cGEcGvTjI1T&limit=5"
 
     $.ajax({
         url: queryURL,
@@ -37,8 +38,7 @@ $(".tag").on("click", function () {
                 title: response.data[i].title,
                 rating: response.data[i].rating,
                 original: response.data[i].images.original.url,
-                still: response.data[i].images.original_still.url,
-                favorited: false
+                still: response.data[i].images.original_still.url
             };
 
             images.list.push(response.data[i].images.original.url);
@@ -50,6 +50,20 @@ $(".tag").on("click", function () {
     });
 });
 
+$("#favoritesDiv").on("click", ".favorite", function () {
+    // images.list.push($(this).val());
+    if (images.hasOwnProperty($(this).val())) {
+        imagePosition = images.list.indexOf($(this).val());
+        updateImages();
+    } else {
+        images.list.push($(this).val());
+        images[$(this).val()] = favorites[$(this).val()];
+        imagePosition = images.list.length - 1;
+        updateImages();
+    }
+});
+
+
 
 //
 //
@@ -57,14 +71,19 @@ $(".tag").on("click", function () {
 //
 //
 function createTag(tag) {
-    var btn = $('<button class="tag">').text(tag+ "  ");
+    var btn = $('<button>').text(tag);
     btn.attr("value", tag);
+    btn.attr("class", "tag");
 
     $("#tagsDiv").append(btn);
 }
 
 function createFavorite(favorite) {
-    var btn = $('<button class="tag"').text("placeholder"); //fix this
+    var btn = $('<button>').text(favorite.title);
+    btn.attr("value", favorite.original);
+    btn.attr("class", "favorite");
+
+    $("#favoritesDiv").append(btn);
 }
 
 function updateImages() {
@@ -74,17 +93,30 @@ function updateImages() {
     } else {
         if (imagePosition > 0 && $("#arrowLeft").hasClass("hide")) {
             $("#arrowLeft").toggleClass("hide");
+        } else if (imagePosition === 0 && !$("#arrowLeft").hasClass("hide")) {
+            $("#arrowLeft").toggleClass("hide");
         }
-    
+
         if (imagePosition < images.list.length - 1 && $("#arrowRight").hasClass("hide")) {
+            $("#arrowRight").toggleClass("hide");
+        } else if (imagePosition === images.list.length - 1 && !$("#arrowRight").hasClass("hide")) {
             $("#arrowRight").toggleClass("hide");
         }
     }
 
+    var currentImage = images[images.list[imagePosition]];
+
+    if (favorites.list.indexOf(currentImage.original) !== -1) {
+        $("#likeBtn").attr("class", "fa-2x fas fa-heart")
+    } else {
+        $("#likeBtn").attr("class", "fa-2x far fa-heart")
+    }
+
+
     var img = $("<img>");
-    
+
     img.attr("src", images[images.list[imagePosition]].original);
-    
+
     $("#images").empty();
     $("#images").append(img);
     updateInfo();
@@ -95,15 +127,21 @@ function updateInfo() {
     var rating = $("<p>");
     var original = $("<p>");
     var originalLink = $("<a>");
-    
-    originalLink.attr("href", images[images.list[imagePosition]].original);
-    originalLink.attr("target", "blank_");
-    originalLink.text(images[images.list[imagePosition]].original);
 
-    title.append("<b>Title:</b> " + images[images.list[imagePosition]].title);
-    rating.append("<b>Rating:</b> " + images[images.list[imagePosition]].rating)
-    original.append("<b>Original:</b> ");
-    original.append(originalLink);
+    if (imagePosition !== "empty") {
+        originalLink.attr("href", images[images.list[imagePosition]].original);
+        originalLink.attr("target", "blank_");
+        originalLink.text(images[images.list[imagePosition]].original);
+
+        title.append("<b>Title:</b> " + images[images.list[imagePosition]].title);
+        rating.append("<b>Rating:</b> " + images[images.list[imagePosition]].rating)
+        original.append("<b>Original:</b> ");
+        original.append(originalLink);
+    } else {
+        title.append("<b>Title: </b>");
+        rating.append("<b>Rating: </b>");
+        original.append("<b>Original: </b>");
+    }
 
     $("#imageInfo div").empty();
 
--- assets/javascript/buttons.js
+++ assets/javascript/buttons.js
@@ -2,6 +2,21 @@ var tagsScroll = 0;
 var favsScroll = 0;
 
 $("header .fa-grip-lines").on("click", function () {
+    if ($("#tagsHolder").is(":visible")) {
+        $("#tagsHolder").animate({ width: "toggle" }, 250);
+    }
+    if ($("#favoritesDiv").is(":visible")) {
+        $("#favoritesDiv").animate({ width: "toggle" }, 250);
+    }
+    if ($("#addForm").is(":visible")) {
+        $("#addForm").animate({ width: "toggle" }, 250);
+    }
+    if ($("#deleteForm").is(":visible")) {
+        $("#deleteForm").animate({ width: "toggle" }, 250);
+    }
+
+    clearInputs();
+
     $("#topBar").slideToggle(250);
 });
 
@@ -10,17 +25,33 @@ $("#imageInfo .fa-grip-lines").on("click", function () {
 });
 
 $("#tagsBtn").on("click", function () {
-    if ($("#favoritesDiv").is(":visible") === true) {
+    if ($("#favoritesDiv").is(":visible")) {
         $("#favoritesDiv").animate({ width: "toggle" }, 250);
     }
+    if ($("#addForm").is(":visible")) {
+        $("#addForm").animate({ width: "toggle" }, 250);
+    }
+    if ($("#deleteForm").is(":visible")) {
+        $("#deleteForm").animate({ width: "toggle" }, 250);
+    }
 
-    $("#tagsHolder").animate({ width: 'toggle' }, 1000);
+    clearInputs();
+
+    $("#tagsHolder").animate({ width: 'toggle' }, 250);
 });
 
 $("#favoritesBtn").on("click", function () {
-    if ($("#tagsDiv").is(":visible") === true) {
+    if ($("#tagsDiv").is(":visible")) {
         $("#tagsHolder").animate({ width: 'toggle' }, 250);
     }
+    if ($("#addForm").is(":visible")) {
+        $("#addForm").animate({ width: "toggle" }, 250);
+    }
+    if ($("#deleteForm").is(":visible")) {
+        $("#deleteForm").animate({ width: "toggle" }, 250);
+    }
+
+    clearInputs();
 
     $("#favoritesDiv").animate({ width: 'toggle' }, 250);
 });
@@ -92,10 +123,6 @@ $("#favoritesDiv").mousedown(function (e) {
 $("#arrowLeft").on("click", function () {
     if (imagePosition > 0) {
         imagePosition--;
-
-        if (imagePosition === 0 && !$("#arrowLeft").hasClass("hide")) {
-            $("#arrowLeft").toggleClass("hide");
-        }
     }
 
     updateImages();
@@ -104,23 +131,103 @@ $("#arrowLeft").on("click", function () {
 $("#arrowRight").on("click", function () {
     if (imagePosition < images.list.length - 1) {
         imagePosition++;
-
-        if (imagePosition === images.list.length - 1 && !$("#arrowRight").hasClass("hide")) {
-            $("#arrowRight").toggleClass("hide");
-        }
     }
 
     updateImages();
 });
 
 $("#likeBtn").on("click", function () {
-    // like something
+    var currentImage = images[images.list[imagePosition]];
+
+    if (imagePosition !== "empty") {
+        if (favorites[currentImage.original] === undefined) {
+            favorites.length++;
+            favorites.list.push(currentImage.original);
+            favorites[currentImage.original] = currentImage;
+
+            createFavorite(favorites[currentImage.original]);
+
+            localStorage.setItem("favorites", JSON.stringify(favorites));
+
+            $("#likeBtn").attr("class", "fa-2x fas fa-heart");
+        } else {
+            favorites.length--;
+
+            $(`button[value="${currentImage.original}"]`).remove();
+
+            delete favorites[currentImage.original];
+            favorites.list.splice(favorites.list.indexOf(currentImage.original), 1);
+
+            localStorage.setItem("favorites", JSON.stringify(favorites));
+
+            $("#likeBtn").attr("class", "fa-2x far fa-heart");
+        }
+    }
 });
 
-$("#addTag").on("click", function () {
+$("#addBtn").on("click", function () {
+    if ($("#deleteForm").is(":visible")) {
+        $("#deleteForm").animate({ width: "toggle" }, 250);
+    }
 
+    clearInputs();
+    $("#addForm").animate({ width: "toggle" }, 250);
 });
 
-$("#deleteTag").on("click", function () {
+$("#deleteBtn").on("click", function () {
+    if ($("#addForm").is(":visible")) {
+        $("#addForm").animate({ width: "toggle" }, 250);
+    }
 
+    clearInputs();
+    $("#deleteForm").animate({ width: "toggle" }, 250);
 });
+
+$("#addForm").on("submit", function (e) {
+    e.preventDefault();
+    var tagToAdd = $("#addInput").val().trim();
+
+    if ($("#addInput").val().trim() !== "") {
+        createTag(tagToAdd);
+        tags.push(tagToAdd);
+        $("#addInput").val("");
+
+        localStorage.setItem("tags", JSON.stringify(tags));
+    }
+});
+
+$("#deleteForm").on("submit", function (e) {
+    e.preventDefault();
+    var tagToDelete = $("#deleteInput").val().trim();
+
+    if ($("#deleteInput").val().trim() !== "" && tags.indexOf(tagToDelete) !== -1) {
+        $(`button[value="${tagToDelete}"]`).remove();
+        tags.splice(tags.indexOf(tagToDelete), 1);
+        $("#deleteInput").val("");
+
+        localStorage.setItem("tags", JSON.stringify(tags));
+    }
+});
+
+$("#clearBtn").on("click", function () {
+    $("#images").empty();
+    images = {
+        offset: {},
+        list: []
+    };
+    imagePosition = "empty";
+
+    updateInfo();
+
+    if (!$("#arrowRight").hasClass("hide")) {
+        $("#arrowRight").toggleClass("hide");
+    }
+    if (!$("#arrowLeft").hasClass("hide")) {
+        $("#arrowLeft").toggleClass("hide");
+    }
+});
+
+function clearInputs() {
+    $("#addInput").val("");
+    $("#deleteInput").val("");
+}
